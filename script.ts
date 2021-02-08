a64c43e586d38ec2e28a85f55eaa4efd39cf60c5
Hirosuke <senolynn7584@gmail.com>
2021-02-08 22:01:49 +0900

    軽くリファクタリング

--- script.ts
+++ script.ts
@@ -1,8 +1,11 @@
 const air=["N₂","O₂","Ar","CO₂","Ne","He","Kr","H₂","O₃","I₂"];
 const metal=["Li","Be","Na","Mg","Al","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Cs","Ba","*","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Fr","Ra","*","Rf","Db","Sg","Bh","Hs","Cn","Fl","Uue","Ubn","Uhe","Usn","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr"]
 const element=["H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe","Cs","Ba","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr"]
+
 class Dropper{
+
     constructor(){}
+
     dropAir(amount:number):Array<String>{
         const result=[];
         for(var i:number=0;i<=amount;i++){
@@ -19,6 +22,7 @@ class Dropper{
             else if(n<0.000001){result.push(air[9]);}}
         return result;
     }
+
     dropMetal(amount:number):Array<String>{
         const result=[];
         for(var i:number=0;i<=amount;i++){
