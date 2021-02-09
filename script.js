b34479682fb0347d26d85c8160ed03be7e2a8193
Hirosuke <senolynn7584@gmail.com>
2021-02-09 20:49:23 +0900

    ガチャウィンドウ作成

--- script.js
+++ script.js
@@ -3,8 +3,20 @@ const metal=["Li","Be","Na","Mg","Al","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co"
 const rare=["La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr"];
 const element=["H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe","Cs","Ba","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn","Fr","Ra","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr"];
 
-function openResult(amount){
-    document.querySelector(".result-screen")
+function openAir(){
+    document.querySelector(".air-gacha-screen").classList.toggle("hide");
+}
+
+function openElement(){
+    document.querySelector(".element-gacha-screen").classList.toggle("hide");
+}
+
+function openMetal(){
+    document.querySelector(".metal-gacha-screen").classList.toggle("hide");
+}
+
+function openRare(){
+    document.querySelector(".rare-gacha-screen").classList.toggle("hide");
 }
 
 function dropAir(amount){
